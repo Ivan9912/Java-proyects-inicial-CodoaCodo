c044e4c3c7ec874fec134a65034cb970caa08f85
Ivan Maidana <ivan.maidana9912@gmail.com>
2022-10-14 14:49:02 -0300

    arreglo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -41,15 +43,69 @@ public class Main {
         }*/
 //------------------------------------------------
 
-        for (int i=2; i<=50; i++) {
+        /*for (int i=2; i<=50; i++) {
         if (i%2 == 0) {
             System.out.println("Este es un número par y es el número: " + i);
         }else {
             System.out.println("Aquí yace un número impar...");
         }
-    }
+    }*/
 //-----------------------------------------------------
 
+    int comprarEntradas, valorEntradaMayores, valorEntradaMenores, cantidadDeMenores, totalEntradaParaMenores, totalEntradaParaMayores, valorTotal;
 
-}
+    valorEntradaMayores = 700;
+    valorEntradaMenores = 500;
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Ingrese la cantidad de entradas que comprará, por favor.");
+    comprarEntradas = sc.nextInt();
+
+    System.out.println("Ingrese la cantidad que les corresponden a los menores de 11 años, por favor.\nLas entradas valen para adultos $700 y para menores $500.");
+    cantidadDeMenores = sc.nextInt();
+
+    if ((cantidadDeMenores < comprarEntradas) && (comprarEntradas >= 0) && (cantidadDeMenores >= 0)) {
+        totalEntradaParaMenores = cantidadDeMenores * valorEntradaMenores;
+        totalEntradaParaMayores = (comprarEntradas - cantidadDeMenores) * valorEntradaMayores;
+        valorTotal = totalEntradaParaMenores + totalEntradaParaMayores;
+
+        System.out.println("Solicitó " + comprarEntradas + " entrada/s.\nCon " + cantidadDeMenores + " entrada/s con el precio para menores.\nEl valor a pagar de las entradas para los adultos es de: $" + totalEntradaParaMenores + " pesos.\nY el valor para las entradas de los menores de edad es: $" + totalEntradaParaMayores + " pesos.\nEl valor total a pagar por las entradas es: $" + valorTotal + " pesos.");
+
+    }else {
+        System.out.println("ERROR:\nEscribió algún número negativo.\nLa cantidad de entradas para menores es mayor que la cantidad de entradas solicitadas.\nRevise alguno de estos posibles errores. Por favor vuelva a ingresar a realizar su compra. Gracias por su visita.");
+    }
+
+//---------------------------------------------------------------------------
+
+        /*String nameUser, passwordUser, nameLogIn, passwordLogIn;
+        int count = 3;
+
+        nameLogIn = "user1234";
+        passwordLogIn = "1234ñI";
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese su nombre de Usuario, por favor.");
+        nameUser = sc.nextLine();
+
+        while (nameUser.equals(nameLogIn)) {
+            System.out.println("Ahora " + nameUser + ", ingrese su contraseña, por favor.");
+            passwordUser = sc.nextLine();
+
+            if (passwordUser.equals(passwordLogIn)) {
+                System.out.println("Bienvenido " + nameLogIn + "!");
+                break;
+            }else {
+                count--;
+                System.out.println(nameLogIn + " Ingresó mal la contraseña...\nVuelva a Ingresar, por favor.\nLe quedan " + count + "/3");
+                if (count == 0) {
+                    System.out.println(nameLogIn + " Superaste los 3 intentos permitidos para loggear. Vuelva a intentar!");
+                    break;
+                }
+            }
+
+        }*/
+
+
+
+    }
 }
